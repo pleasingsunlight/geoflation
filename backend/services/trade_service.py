381e5f4d1b0e52d6375927b091e57eaadf3fc712
Priyanshu Kumar <pk6346198@gmail.com>
2026-05-05 17:16:09 +0530

    Add Redis caching for commodity trends and trade network

--- backend/services/trade_service.py
+++ backend/services/trade_service.py
@@ -3,14 +3,20 @@ from datetime import datetime, timedelta
 import random
 from backend.models.schemas import CommodityPoint, CommoditySeries, CommodityTrendsResponse
 from backend.ml_models.price_forecast import forecast_commodity
-
+from backend.utils.cache import get_cache, set_cache
 
 
 def get_trade_network() -> TradeNetworkResponse:
     """
-    Mock trade network (will later be replaced with real data / graph model)
+    Mock trade network (cached)
     """
 
+    # Try cache first
+    cached = get_cache("trade_network")
+    if cached:
+        return TradeNetworkResponse(**cached)
+
+    # Build network
     nodes = [
         TradeNode(id="russia", name="Russia", type="country"),
         TradeNode(id="china", name="China", type="country"),
@@ -24,16 +30,35 @@ def get_trade_network() -> TradeNetworkResponse:
         TradeEdge(source="oil", target="eu", weight=0.8),
     ]
 
-    return TradeNetworkResponse(nodes=nodes, edges=edges)
+    network = {
+        "nodes": [node.dict() for node in nodes],
+        "edges": [edge.dict() for edge in edges],
+    }
+
+    # Store in cache
+    set_cache("trade_network", network)
+
+    return TradeNetworkResponse(**network)
 
 
 def get_commodity_trends() -> CommodityTrendsResponse:
+    cached = get_cache("commodity_trends")
+
+    if cached:
+        return cached
+
     oil = forecast_commodity(80)
     gas = forecast_commodity(50)
     wheat = forecast_commodity(30)
 
-    return CommodityTrendsResponse(trends=[
-        CommoditySeries(commodity="oil", data=oil),
-        CommoditySeries(commodity="gas", data=gas),
-        CommoditySeries(commodity="wheat", data=wheat),
-    ])
+    result = {
+        "trends": [
+            {"commodity": "oil", "data": oil},
+            {"commodity": "gas", "data": gas},
+            {"commodity": "wheat", "data": wheat},
+        ]
+    }
+
+    set_cache("commodity_trends", result)
+
+    return result
